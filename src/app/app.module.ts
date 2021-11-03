c0b4beafbb4a630d36993bc0ff96d7465d9b03c6
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-11-03 12:06:47 +0100

    Try to make a request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SharedModule } from "./shared/shared.module";
 import { FilePageModule } from "./pages/file-page/file-page.module";
 import { RouterModule } from "@angular/router";
 import { SettingsPageComponent } from "./pages/settings-page/settings-page.component";
+import { HttpClientModule } from "@angular/common/http";
 
 
 const routes = [
@@ -27,6 +28,7 @@ const routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     FilePageModule,
     RouterModule.forRoot(routes),
--- src/app/zaak.service.ts
+++ src/app/zaak.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Zaak } from "./zaak.model";
 import { Subject } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZaakService {
+  static readonly BASE_URL = 'https://open-zaak.test.sudwestfryslan.opengem.nl/';
+  static readonly BEARER_URL ='http://openzaakbrug.test.sudwestfryslan.opengem.nl/jwt';
+
   private zaakSubject: Subject<Zaak> = new Subject<Zaak>();
   public zaakObserver = this.zaakSubject.asObservable();
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   // TODO: Get last 20 zaken.
 
   // TODO: Get zaak by id.
   public getZaak(id: string): void {
+    this.http.get(`${ZaakService.BASE_URL}zaken/api/v1/zaken?identificatie=${id}`, {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+        'Accept-Crs' : 'EPSG:4326',
+        'Authorization': 'Bearer: '
+      }),
+    }).subscribe((res) => {
+      console.log(res)
+    })
+
     this.zaakSubject.next({
       id: id,
       type: 'testZaak B1202',
